ec3f0fadb97529f59bc46f074cabb28700817b02
Max <maxeisen98@web.de>
2021-04-16 13:50:29 +0200

    added threadroute

--- api/lib/app.js
+++ api/lib/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const userRoutes = require('../routes/userroutes');
+// imports
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const userRoutes = require('../routes/userroutes')
+const threadRoutes = require('../routes/threadroutes')
 
 // setting up the port of api
-const port = 3000;
+const port = 3000
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/threads', threadRoutes)
 
 app.listen(port, () => {
-  console.log(`App live at http://localhost:${port}`);
-});
+  console.log(`App live at http://localhost:${port}`)
+})
