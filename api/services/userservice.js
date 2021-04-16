d56f98f28b86b5a7256716b5c9a8530f01f0e841
Max <maxeisen98@web.de>
2021-04-16 13:51:04 +0200

    deleted semicolons

--- api/services/userservice.js
+++ api/services/userservice.js
@@ -1,4 +1,4 @@
-const pool = require('../data/connection');
+const pool = require('../data/connection')
 
 const getAllUsers = async () => {
     let result = await pool.query('SELECT * FROM \"Users\"')
@@ -12,10 +12,10 @@ const getAllUsers = async () => {
 const addUser =  async (user) => {
     const result = await pool.query('INSERT INTO \"Users\"(\"Forename\", \"Lastname\", \"Email\", \"Password\") VALUES ($1, $2, $3, $4) RETURNING "Id"', 
     [user.Forename, user.Lastname, user.Email, user.Password])
-    return result.rows[0].Id;
+    return result.rows[0].Id
 }
 
 module.exports = {
     getAllUsers: getAllUsers,
     addUser: addUser
-};
+}
