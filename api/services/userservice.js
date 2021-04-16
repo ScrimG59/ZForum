ceb86918152262cf51ddba9471e049cacfdac281
Max <maxeisen98@web.de>
2021-04-16 16:53:34 +0200

    cleaned up code and added function that gets the user of a certain thread

--- api/services/userservice.js
+++ api/services/userservice.js
@@ -1,7 +1,7 @@
 const pool = require('../data/connection')
 
 const getAllUsers = async () => {
-    let result = await pool.query('SELECT * FROM \"Users\"')
+    let result = await pool.query('SELECT * FROM "Users"')
     return result.rows
 
     /*return pool.query('SELECT * FROM \"Users\"').then((result) => {
@@ -9,13 +9,20 @@ const getAllUsers = async () => {
     })*/
 }
 
+const getUserOfThread = async (userId) => {
+    let result = await pool.query('SELECT * FROM "Users" WHERE "Id" = $1', [userId])
+
+    return result.rows[0]
+}
+
 const addUser =  async (user) => {
-    const result = await pool.query('INSERT INTO \"Users\"(\"Forename\", \"Lastname\", \"Email\", \"Password\") VALUES ($1, $2, $3, $4) RETURNING "Id"', 
+    const result = await pool.query('INSERT INTO "Users"("Forename", "Lastname", "Email", "Password") VALUES ($1, $2, $3, $4) RETURNING "Id"', 
     [user.Forename, user.Lastname, user.Email, user.Password])
     return result.rows[0].Id
 }
 
 module.exports = {
     getAllUsers: getAllUsers,
+    getUserOfThread: getUserOfThread,
     addUser: addUser
 }
