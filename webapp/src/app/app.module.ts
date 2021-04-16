300e4658f25dc65249bc31402485eaf272f40f41
Max <maxeisen98@web.de>
2021-04-16 16:54:22 +0200

    added new components

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ThreadItemComponent } from './thread-item/thread-item.component';
-import { ThreadListComponent } from './thread-list/thread-list.component';
-import { ThreadDetailComponent } from './thread-detail/thread-detail.component';
+import { ThreadItemComponent } from './thread/thread-item/thread-item.component';
+import { ThreadListComponent } from './thread/thread-list/thread-list.component';
+import { ThreadDetailComponent } from './thread/thread-detail/thread-detail.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [
     AppComponent,
-      NavbarComponent,
-      ThreadItemComponent,
-      ThreadListComponent,
-      ThreadDetailComponent
+    NavbarComponent,
+    ThreadItemComponent,
+    ThreadListComponent,
+    ThreadDetailComponent
    ],
   imports: [
     BrowserModule,
