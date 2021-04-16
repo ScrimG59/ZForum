d9a81a69e3ae17ed9c922d13a96a4b6dfcdf2e1c
Max <maxeisen98@web.de>
2021-04-16 16:51:55 +0200

    added the user that created the thread to the response body

--- api/routes/threadroutes.js
+++ api/routes/threadroutes.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const { getAllThreads } = require('../services/threadservice')
+const { getUserOfThread } = require('../services/userservice') 
+const Thread = require('../models/thread') 
 const router = express.Router()
 
+// HTTP-Get to get all Threads
 router.get('/', async (req, res) => {
     let threads = await getAllThreads()
+    let threadList = [];
+
+    for(thread of threads) {
+        const userOfThread = await getUserOfThread(thread.User_Id)
+        const newThread = new Thread(thread.Id, thread.Title, thread.Description, thread.CreationDate, userOfThread, thread.Content)
+
+        threadList.push(newThread)
+    }
 
     return res.status(200).json({
-        Threads: threads
+        Threads: threadList
     })
 })
 
