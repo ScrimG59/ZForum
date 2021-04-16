e4c673621080e9c5d479a20f1b158620f971ff57
Max <maxeisen98@web.de>
2021-04-16 16:54:13 +0200

    added new routes

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ThreadListComponent } from './thread-list/thread-list.component';
+import { ThreadDetailComponent } from './thread/thread-detail/thread-detail.component';
+import { ThreadListComponent } from './thread/thread-list/thread-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const routes: Routes = [
-  {path: '', component: ThreadListComponent}
+  {path: '', component: ThreadListComponent},
+  {path: 'thread-detail/:id', component: ThreadDetailComponent},
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'user-register', component: UserRegisterComponent},
+  {path: 'user-detail/:id', component: UserDetailComponent}
 ];
 
 @NgModule({
