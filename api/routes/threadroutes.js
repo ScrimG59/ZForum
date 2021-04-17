7eeeae26a8d2f0f3d851c0b580fc28ba84b3718d
Max <maxeisen98@web.de>
2021-04-17 15:59:16 +0200

    edited http-get to also get all posts and users of the posts

--- api/routes/threadroutes.js
+++ api/routes/threadroutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const { getAllThreads } = require('../services/threadservice')
-const { getUserOfThread } = require('../services/userservice') 
+const { getUserById } = require('../services/userservice')
+const { getPostsbyThreadId } = require('../services/postservice') 
 const Thread = require('../models/thread') 
+const Post = require('../models/post')
 const router = express.Router()
 
 // HTTP-Get to get all Threads
@@ -10,8 +12,10 @@ router.get('/', async (req, res) => {
     let threadList = [];
 
     for(thread of threads) {
-        const userOfThread = await getUserOfThread(thread.User_Id)
-        const newThread = new Thread(thread.Id, thread.Title, thread.Description, thread.CreationDate, userOfThread, thread.Content)
+        const userOfThread = await getUserById(thread.User_Id)
+        const posts = await getPostsbyThreadId(thread.Id)
+        const postList = await getPosts(posts)
+        const newThread = new Thread(thread.Id, thread.Title, thread.Description, thread.CreationDate, userOfThread, thread.Content, postList)
 
         threadList.push(newThread)
     }
@@ -20,5 +24,21 @@ router.get('/', async (req, res) => {
         Threads: threadList
     })
 })
+
+// helper method to get all posts of a thread
+const getPosts = async (posts) => {
+    let postList = [];
+
+    for(post of posts) {
+        const userOfPost = await getUserById(post.User_Id)
+        const newPost = new Post(post.Id, post.Content, userOfPost, post.CreationDate)
+
+        postList.push(newPost)
+    }
+
+    return postList
+}
+
+router.get('/:id')
 
 module.exports = router
