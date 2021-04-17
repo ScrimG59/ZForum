fee1036b97e55a2d314abfe900547a0fce6acc88
Max <maxeisen98@web.de>
2021-04-17 17:22:09 +0200

    edited

--- api/routes/threadroutes.js
+++ api/routes/threadroutes.js
@@ -13,16 +13,14 @@ router.get('/', async (req, res) => {
 
     for(thread of threads) {
         const userOfThread = await getUserById(thread.User_Id)
-        const posts = await getPostsbyThreadId(thread.Id)
+        const posts = await getPostsbyThreadId(thread.Id) 
         const postList = await getPosts(posts)
-        const newThread = new Thread(thread.Id, thread.Title, thread.Description, thread.CreationDate, userOfThread, thread.Content, postList)
+        const newThread = new Thread(thread.Id, thread.Title, thread.Description, thread.CreationDate, userOfThread.Forename, userOfThread.Id, thread.Content, postList)
 
         threadList.push(newThread)
     }
 
-    return res.status(200).json({
-        Threads: threadList
-    })
+    return res.status(200).json(threadList)
 })
 
 // helper method to get all posts of a thread
@@ -31,7 +29,7 @@ const getPosts = async (posts) => {
 
     for(post of posts) {
         const userOfPost = await getUserById(post.User_Id)
-        const newPost = new Post(post.Id, post.Content, userOfPost, post.CreationDate)
+        const newPost = new Post(post.Id, post.Content, userOfPost.Forename, userOfPost.Id, post.CreationDate)
 
         postList.push(newPost)
     }
