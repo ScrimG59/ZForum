ba9d62970401180dace41d0db65e63414ea1f4ff
Max <maxeisen98@web.de>
2021-04-17 15:55:32 +0200

    include post attribute

--- api/models/thread.js
+++ api/models/thread.js
@@ -1,11 +1,12 @@
 class Thread {
-    constructor(Id, Title, Description, CreationDate, User, Content) {
+    constructor(Id, Title, Description, CreationDate, User, Content, Posts) {
         this.Id = Id;
         this.Title = Title;
         this.Description = Description;
         this.CreationDate = CreationDate;
         this.User = User;
         this.Content = Content;
+        this.Posts = Posts;
     }
 }
 
