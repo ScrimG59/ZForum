9d53ae933c394bb77ee4a07d7691222be84dbe39
Max <maxeisen98@web.de>
2021-04-17 17:25:35 +0200

    added thread-service to communicate with backend

---
+++ webapp/src/services/thread.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Thread } from 'src/models/thread';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllThreads() {
+    return this.http.get('http://localhost:3000/api/thread').pipe(
+      map(data => {
+        const threadArray: Thread[] = [];
+
+        for(const id in data) {
+          threadArray.push(data[id]);
+        }
+
+        return threadArray;
+      })
+    );
+  }
+
+  getThreadById(id: number) {
+    return this.getAllThreads().pipe(
+      map(threadArray => {
+        const result = threadArray.find(thread => thread.Id == id);
+        return result;
+      })
+    );
+  }
+
+}
