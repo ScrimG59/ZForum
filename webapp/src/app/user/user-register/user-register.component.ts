7aa1605b112969b769e30b284ac68b4ebb73c0c0
Max <maxeisen98@web.de>
2021-04-19 20:47:14 +0200

    added functionality

--- webapp/src/app/user/user-register/user-register.component.ts
+++ webapp/src/app/user/user-register/user-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterUser } from 'src/models/RegisterUser';
+import { AlertifyService } from 'src/services/alertify.service';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-user-register',
@@ -7,9 +12,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegisterComponent implements OnInit {
 
-  constructor() { }
+  registrationForm: FormGroup;
+  user: RegisterUser;
+  userSubmitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private userService: UserService,
+              private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.createRegistrationForm();
   }
+
+  createRegistrationForm() {
+    this.registrationForm = this.formBuilder.group({
+      Forename: new FormControl(null, Validators.required),
+      Lastname: new FormControl(null, Validators.required),
+      Email: new FormControl(null, [Validators.required, Validators.email]),
+      Password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      ConfirmPassword: new FormControl(null, Validators.required)
+    });
+
+    this.registrationForm.setValidators(this.passwordMatchingValidator);
+  }
+
+  // a custom validator that checks if the content of "confirmPassword"-FormControl matches the content of "userPassword"-FormControl
+  passwordMatchingValidator(formGroup: FormGroup): Validators {
+    return formGroup.get('Password').value === formGroup.get('ConfirmPassword').value ? null : {notMatched: true};
+  }
+
+  onSubmit() {
+    this.userSubmitted = true;
+
+    if(this.registrationForm.valid) {
+      this.userService.registerUser(this.createUser()).subscribe(() => {
+        this.registrationForm.reset();
+        this.userSubmitted = false;
+        this.alertifyService.success('Successfully registered!');
+      });
+    }
+  }
+
+  onBack() {
+    this.router.navigate(['']);
+  }
+
+  createUser() {
+    return this.user = {
+      Forename: this.Forename.value,
+      Lastname: this.Lastname.value,
+      Email: this.Email.value,
+      Password: this.Password.value
+    }
+  }
+
+  // ------------------------------------
+  // Getter-methods for all form controls
+  // ------------------------------------
+
+  get Forename() {
+    return this.registrationForm.get('Forename') as FormControl;
+  }
+
+  get Lastname() {
+    return this.registrationForm.get('Lastname') as FormControl;
+  }
+
+  get Password() {
+    return this.registrationForm.get('Password') as FormControl;
+  }
+
+  get ConfirmPassword() {
+    return this.registrationForm.get('ConfirmPassword') as FormControl;
+  }
+
+  get Email() {
+    return this.registrationForm.get('Email') as FormControl;
+  }
+  // ------------------------------------
+
 
 }
