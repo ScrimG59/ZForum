31dc199dca7d9dd38ff0f93e6ee041e5d9a6dbc0
Max <maxeisen98@web.de>
2021-04-19 20:44:54 +0200

    added functionality

--- webapp/src/app/user/user-login/user-login.component.ts
+++ webapp/src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertifyService } from 'src/services/alertify.service';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private userService: UserService,
+              private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      Email: new FormControl(null, [Validators.required, Validators.email]),
+      Password: new FormControl(null, [Validators.required])
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  onBack() {
+    this.router.navigate(['']);
+  }
+
+
+   // ------------------------------------
+  // Getter-methods for all form controls
+  // ------------------------------------
+
+  get Email() {
+    return this.loginForm.get('Email') as FormControl;
+  }
+
+  get Password() {
+    return this.loginForm.get('Password') as FormControl;
+  }
+
+  // ------------------------------------
 
 }
