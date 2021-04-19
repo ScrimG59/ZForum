dd6f277bc83708acb4c0c769e8ca020603dc04e2
Max <maxeisen98@web.de>
2021-04-19 20:40:24 +0200

    added getThreadById function

--- api/services/threadservice.js
+++ api/services/threadservice.js
@@ -5,6 +5,12 @@ const getAllThreads = async () => {
     return result.rows
 }
 
+const getThreadById = async (id) => {
+    let result = await pool.query('SELECT * FROM "Threads" WHERE "Id" = $1', [id])
+    return result.rows[0]
+}
+
 module.exports = {
-    getAllThreads: getAllThreads
+    getAllThreads: getAllThreads,
+    getThreadById: getThreadById
 }
