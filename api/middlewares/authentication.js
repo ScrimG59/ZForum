e1f485644466fb5ae99cd1b3c0fe44d03f339739
Max <maxeisen98@web.de>
2021-04-20 21:30:23 +0200

    new middleware for authentication via json web token

---
+++ api/middlewares/authentication.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  // if there's an authHeader split it, so we only get the token
+  // otherwise return undefined
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if(token === null) {
+      return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if(err) {
+          return res.sendStatus(403)
+      } 
+      req.body = user
+      console.log(user)
+      next()
+  })
+}
+
+module.exports = authenticate
