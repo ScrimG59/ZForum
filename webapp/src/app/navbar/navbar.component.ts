4917b2efbc57b714148f70dc15b3735c50f89acd
Max <maxeisen98@web.de>
2021-04-20 22:31:52 +0200

    added functionality to logout button

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RefreshToken } from 'src/models/RefreshToken';
+import { AlertifyService } from 'src/services/alertify.service';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +14,7 @@ export class NavbarComponent implements OnInit {
   loggedInUser: boolean = false;
   loggedInUserName: string = "";
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -23,11 +27,20 @@ export class NavbarComponent implements OnInit {
   }
 
   onLogout() {
+    const refreshToken = new RefreshToken();
+    refreshToken.RefreshToken = localStorage.getItem('refreshToken');
+    this.userService.logoutUser(refreshToken).subscribe(data => {
+      this.alertifyService.success('Successfully logged out.');
+    }, error => {
+      this.alertifyService.error('Error while logging out.');
+    });
     localStorage.removeItem('token');
     localStorage.removeItem('username');
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('id');
     this.loggedInUser = false;
+    this.router.navigate(['']);
+
   }
 
   onMouseClick() {
