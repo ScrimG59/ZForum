59593bab4523178df2ccca9f9da22be7f57ddf70
Max <maxeisen98@web.de>
2021-04-20 21:35:18 +0200

    added dropdown menu functionality

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  loggedInUser: boolean = false;
+  loggedInUserName: string = "";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loggedIn() {
+    if(localStorage.getItem('username')) {
+      this.loggedInUserName = localStorage.getItem('username').toString();
+      return this.loggedInUser = true
+    }
+  }
+
+  onLogout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('id');
+    this.loggedInUser = false;
+  }
+
+  onMouseClick() {
+    if(!document.querySelector('.dropdown-menu').classList.contains('open')) {
+      document.querySelector('.dropdown-menu').classList.add('open');
+    }
+  }
+
+  onMouseLeave() {
+    if(document.querySelector('.dropdown-menu').classList.contains('open')) {
+      document.querySelector('.dropdown-menu').classList.remove('open');
+    }
   }
 
 }
