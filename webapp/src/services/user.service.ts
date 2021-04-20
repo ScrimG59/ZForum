93ba9e270e0f17124a99657100924f88f372e20c
Max <maxeisen98@web.de>
2021-04-20 22:33:02 +0200

    added logout method to api endpoint

--- webapp/src/services/user.service.ts
+++ webapp/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginUser } from 'src/models/LoginUser';
+import { RefreshToken } from 'src/models/RefreshToken';
 import { RegisterUser } from 'src/models/RegisterUser';
 
 @Injectable({
@@ -11,11 +12,14 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   registerUser(body: RegisterUser) {
-    return this.http.post('http://localhost:3000/api/user/register', body)
+    return this.http.post('http://localhost:3000/api/user/register', body);
   }
 
   loginUser(body: LoginUser) {
-    return this.http.post('http://localhost:3000/api/user/login', body)
+    return this.http.post('http://localhost:3000/api/user/login', body);
   }
 
+  logoutUser(token: RefreshToken) {
+    return this.http.post('http://localhost:3000/api/user/logout', token);
+  }
 }
