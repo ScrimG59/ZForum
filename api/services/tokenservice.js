b8d244b46d30f4f719f49d8189a9f3f7ace93d7c
Max <maxeisen98@web.de>
2021-04-20 21:33:26 +0200

    CRUD functions for refresh tokens

---
+++ api/services/tokenservice.js
@@ -0,0 +1,22 @@
+const pool = require('../data/connection')
+
+const addRefreshToken = async (refreshToken) => {
+    let result = await pool.query('INSERT INTO "RefreshTokens" VALUES ($1) RETURNING "Token"', [refreshToken])
+    return result.rows[0].Token
+}
+
+const deleteRefreshToken = async(refreshToken) => {
+    let result = await pool.query('DELETE FROM "RefreshToken" WHERE "Token" = $1', [refreshToken])
+    return result.rows[0]
+}
+
+const getRefreshToken = async (refreshToken) => {
+    let result = await pool.query('SELECT * FROM "RefreshTokens" WHERE "Token" = $1', [refreshToken])
+    return result.rows[0].Token
+}
+
+module.exports = {
+    addRefreshToken,
+    deleteRefreshToken,
+    getRefreshToken
+}
