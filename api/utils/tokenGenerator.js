a881aaec77b8ceca117e00cec300deeb5f1deadc
Max <maxeisen98@web.de>
2021-04-21 03:24:31 +0200

    token now expires in 10mins

--- api/utils/tokenGenerator.js
+++ api/utils/tokenGenerator.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const generateAccessToken = (user) => {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '20s'})
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '600s'})
 }
 
 module.exports = generateAccessToken
