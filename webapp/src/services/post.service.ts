3003fbb71f70e8811b35c42aa35bca4df21e6d88
Max <maxeisen98@web.de>
2021-04-21 16:39:57 +0200

    new service for posts

---
+++ webapp/src/services/post.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CreatePost } from 'src/models/CreatePost';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  header: HttpHeaders = new HttpHeaders();
+
+  constructor(private http: HttpClient) { }
+
+  addPost(post: CreatePost) {
+    this.header = this.header.set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.post('http://localhost:3000/api/post/add', post, {'headers': this.header});
+  }
+}
