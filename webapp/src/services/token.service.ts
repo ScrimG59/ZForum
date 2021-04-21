9654cb05ef4d805ef6c5cbd71b89689bcb80d067
Max <maxeisen98@web.de>
2021-04-21 16:39:47 +0200

    added typing

--- webapp/src/services/token.service.ts
+++ webapp/src/services/token.service.ts
@@ -9,7 +9,7 @@ export class TokenService {
 
   constructor(private http: HttpClient) { }
 
-  getNewToken(refreshToken) {
+  getNewToken(refreshToken: string) {
     return this.http.post('http://localhost:3000/api/token', {'RefreshToken': localStorage.getItem('refreshToken')}).pipe(
       map(data => {
         return data;
