8ebfdae59fff63f7016275789a2adff01b80bf5b
Max <maxeisen98@web.de>
2021-04-23 00:06:34 +0200

    changed the way how the method retrieves certain infos

--- webapp/src/app/navbar/navbar.component.ts
+++ webapp/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { RefreshToken } from 'src/models/RefreshToken';
 import { AlertifyService } from 'src/services/alertify.service';
+import { TokenService } from 'src/services/token.service';
 import { UserService } from 'src/services/user.service';
 
 @Component({
@@ -14,30 +14,30 @@ export class NavbarComponent implements OnInit {
   loggedInUser: boolean = false;
   loggedInUserName: string = "";
 
-  constructor(private userService: UserService, private router: Router, private alertifyService: AlertifyService) { }
+  constructor(private userService: UserService,
+              private router: Router,
+              private alertifyService: AlertifyService,
+              private tokenService: TokenService) { }
 
   ngOnInit() {
   }
 
   loggedIn() {
-    if(localStorage.getItem('username')) {
-      this.loggedInUserName = localStorage.getItem('username').toString();
+    if(localStorage.getItem('token') && localStorage.getItem('refreshToken')) {
+      this.loggedInUserName = this.tokenService.getInfo().Username;
       return this.loggedInUser = true;
     }
   }
 
   onLogout() {
-    const refreshToken = new RefreshToken();
-    refreshToken.RefreshToken = localStorage.getItem('refreshToken');
+    const refreshToken: string = localStorage.getItem('refreshToken');
     this.userService.logoutUser(refreshToken).subscribe(data => {
       this.alertifyService.success('Successfully logged out.');
     }, error => {
       this.alertifyService.error('Error while logging out.');
     });
     localStorage.removeItem('token');
-    localStorage.removeItem('username');
     localStorage.removeItem('refreshToken');
-    localStorage.removeItem('id');
     this.loggedInUser = false;
     this.router.navigate(['']);
 
@@ -55,4 +55,7 @@ export class NavbarComponent implements OnInit {
     }
   }
 
+  getUserName(): string {
+    return this.tokenService.getInfo().Username;
+  }
 }
