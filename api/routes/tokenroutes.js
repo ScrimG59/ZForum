f1b7272ae9c43fc2204c238e9a994e1420802d81
Max <maxeisen98@web.de>
2021-04-23 00:03:09 +0200

    fixed name of object

--- api/routes/tokenroutes.js
+++ api/routes/tokenroutes.js
@@ -6,6 +6,7 @@ const { getRefreshToken } = require('../services/tokenservice')
 const jwt = require('jsonwebtoken')
 const generateAccessToken = require('../utils/tokenGenerator')
 
+// generates new token if refreshToken in body is in database
 router.post('/', async (req, res) => {
     const refreshToken = req.body.RefreshToken
 
@@ -24,10 +25,26 @@ router.post('/', async (req, res) => {
             return res.status(401).send('Refresh token not valid.')
         }
 
-
-        const accesstoken = generateAccessToken({name: user.Username})
+        const accesstoken = generateAccessToken({Id: user.Id, Username: user.Username})
         return res.status(201).json(accesstoken)
     })
+})
+
+// checks if refreshtoken is in database
+router.post('/refreshtoken', async (req, res) => {
+    const refreshToken = req.body.RefreshToken
+
+    if(!refreshToken) {
+        return res.status(401).send('No Token in header.')
+    }
+
+    const tokenFromDb = await getRefreshToken(refreshToken)
+
+    if(tokenFromDb != refreshToken) {
+        return res.status(401).send('Tokens dont match.')
+    }
+
+    return res.status(200).json('Ok')
 })
 
 module.exports = router
