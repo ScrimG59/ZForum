52a2b4d83db268b941df718f55c42999bcf3bac1
Max <maxeisen98@web.de>
2021-04-23 00:12:17 +0200

    now checks if given token is valid

--- webapp/src/services/auth-guard.service.ts
+++ webapp/src/services/auth-guard.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { AlertifyService } from './alertify.service';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate{
+export class AuthenticationGuardService implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private alertifyService: AlertifyService,
+              private tokenService: TokenService) { }
 
   canActivate(): boolean {
-    if(!localStorage.getItem('token')) {
-      this.router.navigate(['']);
+
+    if(!localStorage.getItem('token') || !localStorage.getItem('refreshToken')) {
+      this.alertifyService.error('You have to login for this action!');
+      this.router.navigate(['user/login']);
       return false;
     }
+
+    // verify if token is valid
+    this.tokenService.checkRefreshToken(localStorage.getItem('refreshToken')).subscribe((status: string) => {
+      if(status) {
+        return true;
+      }
+      return false;
+    })
     return true;
   }
 
