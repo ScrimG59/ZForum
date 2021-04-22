0503c9a539aae0a2c2f14e7a474a2737f30625cf
Max <maxeisen98@web.de>
2021-04-23 00:02:16 +0200

    added new route

--- api/lib/app.js
+++ api/lib/app.js
@@ -6,6 +6,7 @@ const userRoutes = require('../routes/userroutes')
 const threadRoutes = require('../routes/threadroutes')
 const tokenRoutes = require('../routes/tokenroutes')
 const postRoutes = require('../routes/postroutes')
+const authenticateRoutes = require('../routes/authenticateroutes')
  
 // setting up the port of api
 const port = 3000
@@ -19,6 +20,7 @@ app.use('/api/user', userRoutes)
 app.use('/api/thread', threadRoutes)
 app.use('/api/token', tokenRoutes)
 app.use('/api/post', postRoutes)
+app.use('/api/authenticate', authenticateRoutes)
 
 app.listen(port, () => {
   console.log(`App live at http://localhost:${port}`)
